28dc37415552779a05fd930721d62f3d5441cf6e
maluhybiblidev <maluly.biblidev@gmail.com>
2022-11-13 20:39:55 -0300

    Biblidev App

--- app/src/main/java/com/accao/biblidevapp/MainActivity.java
+++ app/src/main/java/com/accao/biblidevapp/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient( new WebViewClient() );
 
         webView.getSettings().setJavaScriptEnabled(true);
+
         webView.getSettings().setDomStorageEnabled(true);
 
         webView.loadUrl("https://obscure-atoll-49504.herokuapp.com/");
